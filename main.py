c158854da43a86642cdb420388d0ddcfb0e264e9
WunschK <wunsch.kevin@gmail.com>
2021-04-26 17:34:07 +0200

    functions to download all texts and a single one!

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Tutorial zur Nutzung der REST-Schnittstellen von wdb+ am Beispiel des Projekts Religiöse Friedenswahrung und Friedensstiftung in Europa
 
-import urllib.request
 
-# Wie in Python üblich müssen zunächst die notwendigen Pakete installiert werden.
-# Hier zunächst das Paket requests. Dieses Paket muss meist separat installiert werden.
-# # Dieser Schritt wird nicht in diesem Tutorial behandelt!
 import requests as r
 
-# GLOBALE Variable URL festgelegt
+# Wie in Python üblich müssen zunächst die notwendigen Pakete installiert werden.
+# Hier das Paket requests. Dieses Paket muss meist separat installiert werden.
+# # Dieser Schritt wird nicht in diesem Tutorial behandelt!
+
+
+# GLOBALE Variable URL festgelegt - Globale Variablen werden all-caps geschrieben. Diese vermeiden Wiederholungen im Code.
 URL = "https://exist.ulb.tu-darmstadt.de:8443/exist/restxq/edoc"
 
 # Mithilfe von requests führen wir einen get-request aus, der uns alle Einträge aus der collection unter der id "religionsfrieden" ausgibt
@@ -27,24 +28,25 @@ data = response.json()
 # print(data)
 
 
+
 def get_all_texts ():
     for i in range(0, len(data['resources']) - 1):
         RESOURCES = f"{data['resources'][i]['@id']}"
         url = f"{URL}/resource/{RESOURCES}"
         # unter diesem Kommentar wird der Speicherpfad und die Benennung festgelegt. Wir haben uns für die Bennenung nach ID entschieden! Abgespeichert wird es im Ordner, in welchem main.py abgespeichert ist.
-        urllib.request.urlretrieve(url, f"{RESOURCES}.xml")
+        file = r.get(url, allow_redirects=True)
+        open(f"{RESOURCES}.xml", 'wb').write(file.content)
         i += 1
 
-
-# Möchte ich nur ein einzelnes Element herunterladen und
-# nach der ID bennenen muss ich den oberen for-loop auskommentieren und unten die 0 durch die Position des gewünschten
-# Textes ersetzen. Python zählt ab 0!
 
 def get_a_single_text ():
     num = int(input(
         f"Welchen Text möchtest du herunterladen? Gib eine Position ein. \n Hier wird ausgeglichen, dass Python bei 0 zu zählen beginnt. "))
     url = f"{URL}/resource/{data['resources'][num - 1]['@id']}"
-    urllib.request.urlretrieve(url, f"{data['resources'][num - 1]['@id']}.xml")
+    file = r.get(url, allow_redirects=True)
+    open(f"{data['resources'][num - 1]['@id']}.xml", 'wb').write(file.content)
+
+# Um entweder alle oder einen Text herunterzuladen unten die Funktion auskommentieren.
 
 # get_all_texts()
 # get_a_single_text()
